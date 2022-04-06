9ba6ef858f454135cdeb92e34c77b9ebf920cbfa
Fernando Garcia Huerta <fernando.garcia.fg6@businesspartner.roche.com>
2022-04-06 14:03:24 +0200

    feat(space): implements clearing space

--- logic/entities/spaces/clearing.js
+++ logic/entities/spaces/clearing.js
@@ -3,6 +3,7 @@ const { FixedGood } = require('../fixed-good.js');
 const { NEXUS, GOODS } = require('../../constants.js');
 const { Space } = require('./space.js');
 const { GoodsAction } = require('../../actions/goods-action.js');
+const { TweenTileAction } = require('../../actions/tween-tile-action.js');
 
 exports.Clearing = class extends Space {
     constructor() {
@@ -11,10 +12,36 @@ exports.Clearing = class extends Space {
         const action1 = new GoodsAction();
         action1.goods.push(new StockableGood(GOODS.WOOD, 2, 2));
 
-        const action2 = new GoodsAction();
-        action2.goods.push(new FixedGood(GOODS.MEADOW_FIELD, 1));
+        const action2 = new TweenTileAction('mf');
 
         this.actions.push(action1);
         this.actions.push(action2);
+    }
+
+    /**
+     * Use Clearing space
+     *
+     * Expected spaceParams:
+     * ```json
+     * {
+     *     actionParams: [   // spaceParams
+     *         { index: 0 }, // index of action to execute first
+     *         {
+     *             index: 1, // index of action to execute second
+     *             actionParams: {
+     *                 cell: 17,        // cell index
+     *                 direction: "t"   // direction of tween tile
+     *             }
+     *         }
+     *     ]
+     * }
+     * ```
+     * @param {*} dwarf       dwarf to be used
+     * @param {*} spaceParams params of the drift-mining space
+     *
+     */
+    use(dwarf, spaceParams) {
+        console.log('Clearing.use', dwarf.id, spaceParams);
+        super.use(dwarf, spaceParams);
     }
 }
